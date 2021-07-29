e5d4ad4a3ef6c44b238e116c2d0989fed2862c88
Haldgerd <haldgerd.code@gmail.com>
2021-07-29 09:40:47 +0200

    "Commentary fixed, minimum refactoring."

--- BlackjackGame-simple/index.js
+++ BlackjackGame-simple/index.js
@@ -1,42 +1,35 @@
 // variable initialization
-
 let wager = document.getElementsByClassName('betWager')[0];
 let cast = document.getElementsByClassName('casting')[0];
 let cardDisplay = document.getElementsByClassName('cards')[0];
-let cardONe = Math.floor((Math.random() * 12) + 1);
-let cardTwo = Math.floor((Math.random() * 12) + 1);
 let array = [];
 
 
 // app functionallity
-
 function setInitial() {
   array = [];
-  console.log(array);
-  console.log(cardDisplay);
+  // console.log(array);
+  // console.log(cardDisplay);
   cardDisplay.innerHTML = "Cards: ";
- 
- 
-  document.getElementsByClassName('sum')[0].innerHTML = "Sum: "; //inital valueMath.floor((Math.random() * 12) + 1);
+  document.getElementsByClassName('sum')[0].innerHTML = "Sum: "; 
   cast.innerHTML = "Play again! Press start game.";
+
   dealCards();
 }
 
-
 function sum() {
   let total = 0;
-  for (let i in array) {
+  for (let i = 0; i < array.length; i++) {
     total += array[i];
-    console.log(array);
-    console.log(i);
+    // console.log(array);
   }
   return total;
 }
 
-
 function dealCards() {
-  cardONe = Math.floor((Math.random() * 12) + 1);
-  cardTwo = Math.floor((Math.random() * 12) + 1);
+  let cardONe = Math.floor((Math.random() * 12) + 1);
+  let cardTwo = Math.floor((Math.random() * 12) + 1);
+
   array.push(cardONe, cardTwo);
   for (let i in array) {
     cardDisplay.innerHTML += " " + array[i];
@@ -77,5 +70,5 @@ function start() {
   }
 }
 
-
+// init main app functionallity. Program.
 dealCards();
