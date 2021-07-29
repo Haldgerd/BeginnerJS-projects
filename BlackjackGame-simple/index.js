d174df0199589947b34833648f8fc9cb74c1dbd7
Haldgerd <haldgerd.code@gmail.com>
2021-07-29 09:15:24 +0200

    "Bug implementing setting initial values."

--- BlackjackGame-simple/index.js
+++ BlackjackGame-simple/index.js
@@ -2,35 +2,84 @@
 
 let displayCardOne = document.getElementsByClassName('cardOne')[0];
 let displayCardTwo = document.getElementsByClassName('cardTwo')[0];
-const stringArr = ["Try again!", "Better luck next time!", "Very close but not there.", "Don't give up!"]; //use math.random with arr.length to chose a random item in array.  Math.random() * arr.length;
+let wager = document.getElementsByClassName('betWager')[0];
+let cast = document.getElementsByClassName('casting')[0];
+let cardDisplay = document.getElementsByClassName('cards')[0];
+let cardONe = Math.floor((Math.random() * 12) + 1);
+let cardTwo = Math.floor((Math.random() * 12) + 1);
+let array = [];
 
 
 // app functionallity
 
-displayCardOne.innerHTML = "";
-displayCardTwo.innerHTML =  "";
-document.getElementsByClassName('sum')[0].innerHTML = "Sum: "; //inital value
+function setInitial() {
+  array = [];
+  console.log(array);
+  console.log(cardDisplay);
+  displayCardTwo.innerHTML = "";
+  displayCardOne.innerHTML = "";
+ 
+  console.log(document.getElementsByClassName('newCard'));
 
+  for (let i in document.getElementsByClassName('newCard')) {
+    document.getElementsByClassName('newCard')[i].remove();
+  }
+
+  console.log(document.getElementsByClassName('newCard'))
+
+  document.getElementsByClassName('sum')[0].innerHTML = "Sum: "; //inital valueMath.floor((Math.random() * 12) + 1);
+  cast.innerHTML = "Play again! Press start game.";
+}
+
+
+function sum() {
+  let total = 0;
+  for (let i in array) {
+    total += array[i];
+    console.log(array);
+    console.log(i);
+  }
+  return total;
+}
+
+
+function dealCards() {
+  displayCardOne.innerHTML = cardONe;
+  displayCardTwo.innerHTML = cardTwo; 
+  array.push(cardONe, cardTwo);
+}
+  
+function addCard() {    
+    console.log("dealing a new card");
+    let newCard = Math.floor((Math.random() * 12) + 1);
+
+    const newAdd = document.createElement('span');
+    newAdd.classList.add('newCard');
+    const node = document.createTextNode(" " + newCard);
+    newAdd.appendChild(node);
+    cardDisplay.appendChild(newAdd);
+
+    array.push(newCard);
+    start();
+  }
 
 function start() {
-  function sum() {
-  return cardONe + cardTwo;
-  }
-  
-  let cardONe = Math.floor((Math.random() * 12) + 1);
-  let cardTwo = Math.floor((Math.random() * 12) + 1);
   let sumOfCards = sum();
 
-  displayCardOne.innerHTML = cardONe;
-  displayCardTwo.innerHTML = cardTwo;
   document.getElementsByClassName('sum')[0].innerHTML = "Sum: " + sumOfCards;
-
+  
   if (sumOfCards < 21) {
-    document.getElementsByClassName('casting')[0].innerHTML = "Would you like to draw another card?";
+    cast.innerHTML = "Would you like to draw another card?";
   } else if (sumOfCards === 21) {
-    document.getElementsByClassName('casting')[0].innerHTML = "You've got blackjack!";
+    cast.innerHTML = "You've got blackjack!";
+    console.log(wager.innerHTML);
+    setTimeout(setInitial, 4000);
   } else {
-    document.getElementsByClassName('casting')[0].innerHTML = "You're out of the game!";
+    cast.innerHTML = "You're out of the game!";
+    setTimeout(setInitial, 4000);
+
   }
 }
 
+
+dealCards();
