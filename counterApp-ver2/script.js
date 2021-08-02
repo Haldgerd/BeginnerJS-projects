dbfa5f0092e024524e0a8e476296731bb1bafa3d
Haldgerd <haldgerd.code@gmail.com>
2021-08-02 21:40:57 +0200

    "Rough working JS code for counter app."

---
+++ counterApp-ver2/script.js
@@ -0,0 +1,34 @@
+const counterDisplay = document.querySelector(".counter");
+const buttonDecrease = document.querySelector(".decrease");
+const buttonReset = document.querySelector(".reset");
+const buttonIncrease =  document.querySelector(".increase");
+let counterTrack = 0;
+
+
+buttonIncrease.addEventListener("click", function() {
+  counterTrack += 1;
+  counterDisplay.textContent = counterTrack; 
+  if (counterTrack < 0) {
+    counterDisplay.style.color = "red";
+ } else {
+   counterDisplay.style.color = "green";
+ }
+});
+
+buttonReset.addEventListener("click", function() {
+  counterTrack = 0;
+  counterDisplay.textContent = counterTrack;
+  counterDisplay.style.color = "black";
+});
+
+buttonDecrease.addEventListener("click", function() {
+  counterTrack -= 1;
+  counterDisplay.textContent = counterTrack; 
+  if (counterTrack < 0) {
+     counterDisplay.style.color = "red";
+  }
+});
+
+if (counterTrack == 0) {
+  counterDisplay.style.color = "black";
+}
