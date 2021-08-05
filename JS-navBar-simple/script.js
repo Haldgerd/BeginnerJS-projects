8466f51998b3f4c80bbbd33dbadd64f67663525d
Haldgerd <haldgerd.code@gmail.com>
2021-08-05 21:48:33 +0200

    Added additional css rulsets, created resposive menu toggle - display of menu icon at specific viewport size.

---
+++ JS-navBar-simple/script.js
@@ -0,0 +1,7 @@
+// const toggleMenu = document.querySelector(".nav__icon");
+// const linkContainer = document.querySelector(".nav__base");
+
+// toggleMenu.addEventListener("click", () => {
+//   console.log("I've been pressed!");
+//   linkContainer.classList.add("responsive");
+// });
