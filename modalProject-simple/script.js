0efbf9745b57706e8171dbf9fb36263e3c2fd056
Haldgerd <haldgerd.code@gmail.com>
2021-08-06 21:10:52 +0200

    "Completed JS code for toggling the overlay modal. Fixed problems with css styling rulesets, modified html file."

---
+++ modalProject-simple/script.js
@@ -0,0 +1,13 @@
+const openModalBtn = document.querySelector(".modal__button");
+const modalOverlay = document.querySelector(".modal__overlay");
+const modalClose = document.querySelector(".modal__close");
+
+
+openModalBtn.addEventListener("click", () => {
+  modalOverlay.classList.toggle("modal__overlay--visible");
+});
+
+modalClose.addEventListener("click", () => {
+  console.log("hello");
+  modalOverlay.classList.remove("modal__overlay--visible");
+});
