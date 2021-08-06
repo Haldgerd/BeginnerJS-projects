30e93bf59c3450d85597ccd0c2e17230bd0087f9
Haldgerd <haldgerd.code@gmail.com>
2021-08-06 09:43:48 +0200

    "JS for project finished. Smooth transition problem  on vertical menu toggle still remains."

--- JS-navBar-simple/script.js
+++ JS-navBar-simple/script.js
@@ -1,7 +1,8 @@
-// const toggleMenu = document.querySelector(".nav__icon");
-// const linkContainer = document.querySelector(".nav__base");
+const toggleMenu = document.querySelector(".nav__icon");
+const linkContainer = document.querySelector(".nav__base");
+const navBar = document.querySelector(".nav__bar");
 
-// toggleMenu.addEventListener("click", () => {
-//   console.log("I've been pressed!");
-//   linkContainer.classList.add("responsive");
-// });
+toggleMenu.addEventListener("click", () => {
+  linkContainer.classList.toggle("responsive");
+  navBar.classList.toggle("no-shadow");
+});
