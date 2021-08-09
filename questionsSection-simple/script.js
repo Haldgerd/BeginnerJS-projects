e6835baf24edfa850f9d38c4f26bbfc5a486d834
Haldgerd <haldgerd.code@gmail.com>
2021-08-09 10:22:35 +0200

    "Added additional CSS rules to css file (button:hover, button), implemented JS code that opens corresponding container based on which button was clicked. Additionaly it changes the button icon when button is clicked on."

--- questionsSection-simple/script.js
+++ questionsSection-simple/script.js
@@ -1,8 +1,36 @@
-const displayAnwserBtn = document.querySelector("button");
-const hiddenAnwser = document.querySelector(".hidden");
+// const displayAnwserBtn = document.querySelector("button");
+const hiddenAnwser = document.querySelectorAll(".hidden");
+const buttonIcon = document.querySelectorAll("i");
+const displayAnwserBtn = document.querySelectorAll("button");
 
-displayAnwserBtn.addEventListener("click", () => {
-  console.log("im here");
-  hiddenAnwser.classList.toggle("visible");
-});
+console.log(displayAnwserBtn);
 
+// upon button click opens corresponding container and changes corresponding button icon.
+function openCorrespondingContainer(index)  {
+  displayAnwserBtn[index].addEventListener("click", () => {
+    if (hiddenAnwser[index].classList.contains("visible")) { 
+      buttonIcon[index].classList.remove("fa-minus-square");
+      buttonIcon[index].classList.add("fa-plus-square");
+    } else {
+      buttonIcon[index].classList.remove("fa-plus-square");
+      buttonIcon[index].classList.add("fa-minus-square");
+      }
+
+    hiddenAnwser[index].classList.toggle("visible");
+  });
+}
+
+
+// function closeAllOtherContainers() {
+
+// }
+
+
+for (let i = 0; i < displayAnwserBtn.length; i++) {
+  openCorrespondingContainer(i);
+}
+
+
+// displayAnwserBtn.forEach(addEventListener("click", () => {
+//   hiddenAnwser.classList.toggle("visible");
+// }));
