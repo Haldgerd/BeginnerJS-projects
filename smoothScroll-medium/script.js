d37237b7b259003eeb7ca2a6fb23106e0b5c22f3
Haldgerd <haldgerd.code@gmail.com>
2021-08-19 22:06:47 +0200

    "Navbar fixed only at certain position - JS code. Added additional rules to css file - mainly for links (hover effect, line-height)."

--- smoothScroll-medium/script.js
+++ smoothScroll-medium/script.js
@@ -1,6 +1,7 @@
 const hamburgerButton = document.querySelector(".hamburger__menu");
 const navigationContainer = document.querySelector(".nav__container");
 const navigationLinks = document.querySelector(".nav__container__links");
+const links = document.querySelectorAll(".nav__container__link");
 const yearDisplay = document.querySelector(".current__year");
 
 // setting current year dynamically
@@ -8,8 +9,31 @@ let currentYear = new Date ();
 yearDisplay.textContent = currentYear.getFullYear();
 
 
-
+// setting a dynamic menu height(links container)
 hamburgerButton.addEventListener("click", () => {
-  navigationLinks.classList.toggle("nav__links--active");
+  // navigationLinks.classList.toggle("nav__links--active");
   // navigationContainer.style.backgroundColor = `hsl(${157}, ${48}%, ${43}%)`;
+  const containerHeight = navigationLinks.getBoundingClientRect();//returns position of the element relative to viewport and it's dimensions -height, width.
+  //console.log(containerHeight);
+
+  let totalHeight = 0;
+  links.forEach(link => {
+    totalHeight += link.getBoundingClientRect().height;
+  });
+
+  if (containerHeight.height === 0) {
+    navigationLinks.style.height = `${totalHeight}px`
+  } else {
+    navigationLinks.style.height = 0;
+  }
+});
+
+//fixed navbar on scroll, changing it's color
+window.addEventListener("scroll", () => {
+  // pageYOffset returns a integer value in px, value represents in lay terms - how much have we scrolled already.
+  if (window.pageYOffset > navigationContainer.getBoundingClientRect().height) {
+    navigationContainer.classList.add("nav--scroll");
+  } else {
+    navigationContainer.classList.remove("nav--scroll");
+  }
 });
