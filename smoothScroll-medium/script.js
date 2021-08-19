6deab35659df88927a815f4cd3f2e0a9160d9ab0
Haldgerd <haldgerd.code@gmail.com>
2021-08-19 14:58:13 +0200

    "Added id's to sections, added smooth scrolling property to html ruleset, added js code to synamically display current year in footer."

--- smoothScroll-medium/script.js
+++ smoothScroll-medium/script.js
@@ -1,6 +1,13 @@
 const hamburgerButton = document.querySelector(".hamburger__menu");
 const navigationContainer = document.querySelector(".nav__container");
 const navigationLinks = document.querySelector(".nav__container__links");
+const yearDisplay = document.querySelector(".current__year");
+
+// setting current year dynamically
+let currentYear = new Date ();
+yearDisplay.textContent = currentYear.getFullYear();
+
+
 
 hamburgerButton.addEventListener("click", () => {
   navigationLinks.classList.toggle("nav__links--active");
