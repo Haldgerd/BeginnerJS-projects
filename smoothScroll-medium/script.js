1d32446b76da01d79d311b7243bbefbc26889ad8
Haldgerd <haldgerd.code@gmail.com>
2021-08-20 10:48:37 +0200

    "Added smooth scrool functionality to js file, added additional class name to button in html.file,  added commentary to css file, fixed smaller problems with property values."

--- smoothScroll-medium/script.js
+++ smoothScroll-medium/script.js
@@ -4,6 +4,7 @@ const navigationLinks = document.querySelector(".nav__container__links");
 const links = document.querySelectorAll(".nav__container__link");
 const yearDisplay = document.querySelector(".current__year");
 const bouncingPointer = document.querySelector(".landing__pointer");
+const navBar = document.querySelector(".nav__container");
 
 // setting current year dynamically
 let currentYear = new Date ();
@@ -17,7 +18,7 @@ hamburgerButton.addEventListener("click", () => {
   const containerHeight = navigationLinks.getBoundingClientRect();//returns position of the element relative to viewport and it's dimensions -height, width.
   //console.log(containerHeight);
 
-  let totalHeight = 0;
+  let totalHeight = 10;
   links.forEach(link => {
     totalHeight += link.getBoundingClientRect().height;
   });
@@ -46,4 +47,26 @@ window.addEventListener("scroll", () => {
 });
 
 
-//making smooth scroll land exactly at the beginning of the section we want to navigate to.
+//making smooth scroll land exactly at the beginning of the section title we want to navigate to.
+links.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // prevent default scroll behaviour
+    // navigate to specific spot
+    //get the id value
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    // slice extracts a part of a string without modifying original string. IN our case we take the string starting from index 1.
+    console.log(id);
+    const section = document.getElementById(id);
+    let elementTopPosition = section.offsetTop;
+    console.log(elementTopPosition);
+    //offset top returns the position of the element relative to viewport (from top of viewport).
+    
+    window.scrollTo({
+      left: 0,
+      // substract the height of navbar from elements top position, to scroll to the exact position of specific section.
+      top: `${elementTopPosition - navBar.getBoundingClientRect().height}`,
+    });
+    navigationLinks.style.height = 0;
+  });
+});
+// 
